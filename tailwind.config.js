f67e1210824f7d1636f456ae38aff6df235b648f
Bashar403 <w0835015@myscc.ca>
2024-03-20 08:01:56 -0400

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './src/**/*.html', './src/**/*.js' ],
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './*.html' // Add this line to include HTML files in the root directory
+  ],
   theme: {
     extend: {
       fontFamily: {
